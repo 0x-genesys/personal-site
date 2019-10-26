d93412590756f54bbc930f68c120af90207bd953
karan <karan.ahuja2014@gmail.com>
2019-10-26 19:03:14 +0530

    woeking

--- app/data/projects.js
+++ app/data/projects.js
@@ -4,7 +4,7 @@ const data = [
     title: 'Springrole',
     subtitle: 'Blockchain verification platform like Linkedin',
     link: 'https://www.springrole.com',
-    image: '/images/projects/springrole.jpg',
+    image: 'images/projects/springrole.jpg',
     date: '2018-2019',
     desc: 'Linkedin On Blockchain '
       + 'Built using Node.js, EJS, react.js and private chain with POA algorithm '
@@ -12,13 +12,13 @@ const data = [
   }, {
     title: 'Blockwala Wallet: Software',
     subtitle: 'Software blockchain wallet for multiple cryptocurrencies',
-    image: '/images/projects/wallet-soft.png',
+    image: 'images/projects/wallet-soft.png',
     date: '2018',
     desc: 'Onboarded 5000+ users with world class security. Able to send crypto instantly across the planet, borderless instant transactions',
   }, {
     title: 'Blockwala Wallet: Hardware',
     subtitle: 'Hardware wallet (Most secure) , compatible with our software wallet, priced at 25$',
-    image: '/images/projects/wallet-hard.png',
+    image: 'images/projects/wallet-hard.png',
     date: '2018',
     desc: 'Hardware wallet are a big hit among crypto users. They are safer than bank lockers and provide unparalleled security.'
     + 'The real challenge was the cryptography. All parts were ordered from China and assembled by us.',
@@ -26,7 +26,7 @@ const data = [
     title: 'Urbanclap SEO',
     subtitle: 'Search Engine Optimization for Urbanclap',
     link: 'https://www.urbanclap.com/delhi-ncr-plumbers',
-    image: '/images/projects/urbanclap.jpg',
+    image: 'images/projects/urbanclap.jpg',
     date: '2017-2018',
     desc: 'Boosted organic traffic for a now Unicorn startup. Urbanclap has done very well in SEO segment and service segment.',
   },
